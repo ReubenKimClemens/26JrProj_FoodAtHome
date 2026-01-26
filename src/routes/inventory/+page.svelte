15de7a4e3f8b826a51f7f8a2ab5efe872287d519
Maria-Louisa Ching <mjc498@drexel.edu>
2026-01-26 15:52:48 -0500

    ✨Category Icon

--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import CategoryTag from '$lib/components/CategoryTag.svelte';
+    import CategoryIcon from '$lib/components/CategoryIcon.svelte';
     
     let selectedCategory = $state('All');
   </script>
   
   <CategoryTag bind:activeCategory={selectedCategory} />
+  <CategoryIcon bind:activeCategory={selectedCategory} />
