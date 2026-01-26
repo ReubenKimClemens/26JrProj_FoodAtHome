fe0a3a5803abe1b62af5027ce32150e7e26c8af6
Maria-Louisa Ching <mjc498@drexel.edu>
2026-01-25 20:26:56 -0500

    🏷️Category Tag

---
+++ src/routes/inventory/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import CategoryTag from '$lib/components/CategoryTag.svelte';
+    
+    let selectedCategory = $state('All');
+  </script>
+  
+  <CategoryTag bind:activeCategory={selectedCategory} />
