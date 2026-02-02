e34f11b77d25c7103effa5f9eaa1c8b32714384e
ReubenKimClemens <146577227+ReubenKimClemens@users.noreply.github.com>
2026-02-02 02:41:45 -0500

    receipt fix

--- src/lib/api/receipts.js
+++ src/lib/api/receipts.js
@@ -1,5 +1,4 @@
 import { supabase } from '$lib/supabaseClient';
-
 //receipts.js
 /*
  User
@@ -44,6 +43,17 @@ export async function getReceipt(receiptId) {
   return data;
 }
 
+/* Get all receipt id's from a user */
+export async function getAllReceiptIds(userId) {
+  const { data, error } = await supabase
+    .from('receipts')
+    .select('id')
+    .eq('user_id', userId);
+
+  if (error) throw new Error(error.message);
+  return data.map(item => item.id);
+}
+
 /* Delete an entire receipt/ */
 export async function deleteReceipt(receiptId) {
   const { error } = await supabase
@@ -74,11 +84,29 @@ export async function getItemsFromReceipt(receiptId) {
     .from('receipt_items')
     .select('*')
     .eq('receipt_id', receiptId);
-
   if (error) throw new Error(error.message);
   return data;
 }
 
+/* Get all receipts items from a user */
+export async function getAllItemsFromUser(userId) {
+  const { data, error } = await supabase
+    .from('receipt_items')
+    .select('*')
+    .eq('user_id', userId);
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+/* Calculate days since receipt item was added */
+export function getDaysSinceAdded(date) {
+  const currentDate = new Date();
+  const addedDate = new Date(date);
+  const diffInTime = currentDate - addedDate;
+  const diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));
+  console.log('Days since added:', diffInDays);
+  return diffInDays;
+}
 
 /*
  Receipt items
@@ -130,18 +158,31 @@ export async function getItemCategoryCount(userId, category) {
   const { count, error } = await supabase
     .from('receipt_items')
     .select('*', { count: 'exact', head: true })
-    .eq('userId', userId)
+    .eq('user_id', userId)
     .eq('category', category);
 
   if (error) throw new Error(error.message);
   return count ?? 0;
 }
+
+/* Get count from all other categories */
+export async function getOtherCategoriesCount(userId) {
+  const { count, error } = await supabase
+    .from('receipt_items')
+    .select('*', { count: 'exact', head: true })
+    .eq('user_id', userId)
+    .neq('category', 'protein');
+
+  if (error) throw new Error(error.message);
+  return count ?? 0;
+}
+
 /* Get all receipt items from specific category  */
 export async function getItemByCategory(userId, category) {
   const { data, error } = await supabase
     .from('receipt_items')
     .select('category')
-    .eq('userId', userId)
+    .eq('user_id', userId)
     .eq('category', category);
 
   if (error) throw new Error(error.message);
@@ -293,4 +334,17 @@ export async function getSpecificBudget(budgetId) {
 
   if (error) throw new Error(error.message);
   return data;
+}
+
+/* get active budget */
+export async function getActiveBudget(userId) {
+  const { data, error } = await supabase
+    .from('budgets')
+    .select('*')
+    .eq('user_id', userId)
+    .eq('active_budget', true)
+    .single();
+
+  if (error) throw new Error(error.message);
+  return data || null;
 }
